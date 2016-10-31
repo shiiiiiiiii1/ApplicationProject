ff11c4762c0a452b4cd40d8dccb1b1fdaf3ca7a2
shoichi <shou1_822@yahoo.co.jp>
2016-10-31 22:28:10 +0900

    first end ambiguity

--- class_application_project.ino
+++ class_application_project.ino
@@ -5,7 +5,11 @@
 
 Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+int low_acceleration = 10;
+int high_acceleration = 1000;
+
 void setup(){
+  Serial.begin(115200);
   rgbled.begin();
   rgbled.setBrightness(50);   // 明るさ指定(0~255)
   rgbled.setPixelColor(0, rgbled.Color(255, 255, 255));
@@ -18,38 +22,54 @@ void loop(){
   x = analogRead(0);
   y = analogRead(1);
   z = analogRead(2);
-  int x_ave = 503;
-  int y_ave = 510;
-  int z_ave = 704;
-  int small_error = 50;   // 誤差範囲の一番小さい値
-  int medium_error = 100;
-  int large_error = 150;
-  int emergency_error = 300;   // 急な加速度を検知するのに測る誤差の値
+  int small_error = 125;   // 誤差範囲の一番小さい値
+  int large_error = 225;
 
   // フリスビー投げられた時の処理
-  if(x < x_ave - emergency_error || x_ave + emergency_error < x){   // 急激な加速度を検知したら(投げたら)
-    while(1){   // 無限ループ
-      int x_throw_ave = 0;   // 空中で進んでる時の数値の初期化
-      int y_throw_ave = 0;
-      int z_throw_ave = 0;
-      if(x_ave - emergency_error <= x && x <= x_ave + emergency_error){   // 加速度が平均に近い値であったら
-        for(int i = 0; i < 30; i++){   // 加速度の値を取得して30回分の平均を出す
-          x_throw_ave = x_throw_ave + x;
-          y_throw_ave = y_throw_ave + y;
-          z_throw_ave = z_throw_ave + z;
-        }
-        x_throw_ave = x_throw_ave / 30;
-        y_throw_ave = y_throw_ave / 30;
-        z_throw_ave = z_throw_ave / 30;
-/**
-ここにledの判定の制御を記述
-**/
-      } else {   // 急激な加速度を検知したら(キャッチしたら)
-        rgbled.setPixelColor(0, rgbled.Color(255, 255, 255));
-        rgbled.show();
-        break;
-      }
+  if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
+    delay(100);
+    int z_before = analogRead(2);
+    int z_difference = 0;
+    int z_sum = 0;
+    for(int i=0; i<2; i++){
+      delay(50);
+      z = analogRead(2);
+Serial.println(z);
+      z_difference = abs(z_before - z);
+      z_sum = z_sum + z_difference;
+      z_before = z;
+    }
+
+Serial.println(z_sum);
+    if(z_sum < small_error){
+      rgbled.setPixelColor(0, rgbled.Color(0, 0, 255));
+      rgbled.show();
+      return_loop();
+    }
+    if(small_error < z_sum && z_sum < large_error){
+      rgbled.setPixelColor(0, rgbled.Color(255, 255, 0));
+      rgbled.show();
+      return_loop();
+    }
+    if(large_error < z_sum){
+      rgbled.setPixelColor(0, rgbled.Color(255, 0, 0));
+      rgbled.show();
+      return_loop();
     }
   }
+}
 
+void return_loop() {
+  while(1){
+    delay(50);
+    int x = analogRead(0);
+    int y = analogRead(1);
+Serial.println("in loop");
+    if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
+      rgbled.setPixelColor(0, rgbled.Color(255, 255, 255));
+      rgbled.show();
+      delay(200);
+      break;
+    }
+  }
 }
