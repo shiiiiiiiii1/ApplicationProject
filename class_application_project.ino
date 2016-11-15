1ece1c61cb10531e2e8b176f80b5c135c895e656
shoichi <shou1_822@yahoo.co.jp>
2016-11-16 00:35:20 +0900

    code rearrangement

--- class_application_project.ino
+++ class_application_project.ino
@@ -1,82 +1,85 @@
 #include <Adafruit_NeoPixel.h>
 
-#define led_pin 6   // led degital I/O pin
-#define NUMPIXELS 1   // 制御するledの数
+const int num_leds = 1;   // 制御するledの数
+const int led_pin = 6;   // led degital I/O pin
+const int flash_pin = 7;   // flash degital I/O pin
 
-Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(NUMPIXELS, led_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(num_leds, led_pin, NEO_GRB + NEO_KHZ800);
 
-int flash_pin = 7;   // flash degital I/O pin
-int low_acceleration = 10;
-int high_acceleration = 1000;
+int x, y, z;
+int small_error = 100;   // 誤差範囲の一番小さい値(Flash用の誤差の値)
+int medium_error = 125;
+int large_error = 250;
+int low_acceleration = 24;   // 急激な加速度の検知
+int high_acceleration = 1000;   // 急激な加速度の検知
+
 
 void setup(){
-  Serial.begin(115200);
   rgbled.begin();
-  rgbled.setBrightness(50);   // 明るさ指定(0~255)
-  rgbled.setPixelColor(0, rgbled.Color(255, 255, 255));
-  rgbled.show();   // 反映
+  for(int i=0; i<=num_leds; i++){
+    rgbled.setBrightness(50);   // 明るさ指定(0~255)
+    rgbled.setPixelColor(i, rgbled.Color(255, 255, 255));
+    rgbled.show();   // 反映
+  }
   pinMode(flash_pin, OUTPUT);
   digitalWrite(flash_pin, LOW);
 }
 
 void loop(){
-  long x, y, z;
-  x = y = z = 0;
   x = analogRead(0);
   y = analogRead(1);
   z = analogRead(2);
-  int small_error = 100;   // 誤差範囲の一番小さい値(Flash用の誤差の値)
-  int medium_error = 125;
-  int large_error = 250;
 
   // フリスビー投げられた時の処理
   if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
     delay(100);
-    int z_before = analogRead(2);
+    int z_before_val = analogRead(2);
     int z_difference = 0;
     int z_sum = 0;
     for(int i=0; i<2; i++){
       delay(50);
       z = analogRead(2);
-Serial.println(z);
-      z_difference = abs(z_before - z);
-      z_sum = z_sum + z_difference;
-      z_before = z;
+      z_difference = abs(z_before_val - z);
+      z_sum += z_difference;
+      z_before_val = z;
     }
 
-Serial.println(z_sum);
     if(z_sum < medium_error){
-      rgbled.setPixelColor(0, rgbled.Color(0, 0, 255));
-      rgbled.show();
-      if(z_sum < small_error){
+      change_ledcolor(0, 0, 255);
+      if(z_sum < small_error){   // フラッシュ判定
         delay(50);
         digitalWrite(flash_pin, HIGH);
+        delay(50);
         digitalWrite(flash_pin, LOW);
       }
       return_loop();
     }
     if(medium_error < z_sum && z_sum < large_error){
-      rgbled.setPixelColor(0, rgbled.Color(255, 255, 0));
-      rgbled.show();
+      change_ledcolor(255, 255, 0);
       return_loop();
     }
     if(large_error < z_sum){
-      rgbled.setPixelColor(0, rgbled.Color(255, 0, 0));
-      rgbled.show();
+      change_ledcolor(255, 0, 0);
       return_loop();
     }
+  }
+}
+
+
+void change_ledcolor(int r, int g, int b) {
+  for(int i=0; i<=num_leds; i++){
+    rgbled.setPixelColor(i, rgbled.Color(r, g, b));
+    rgbled.show();
   }
 }
 
 void return_loop() {
   while(1){
     delay(50);
-    int x = analogRead(0);
-    int y = analogRead(1);
-Serial.println("in loop");
+    x = analogRead(0);
+    y = analogRead(1);
     if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
-      rgbled.setPixelColor(0, rgbled.Color(255, 255, 255));
-      rgbled.show();
+      change_ledcolor(255, 255, 255);
       delay(200);
       break;
     }
