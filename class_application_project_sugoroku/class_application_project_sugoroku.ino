df554de142ecd97777e4ee9a42d61807e5f0dd37
shoichi <shou1_822@yahoo.co.jp>
2016-11-17 16:10:44 +0900

    edit mode sugoroku

--- class_application_project_sugoroku/class_application_project_sugoroku.ino
+++ class_application_project_sugoroku/class_application_project_sugoroku.ino
@@ -8,12 +8,11 @@ Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(num_leds, led_pin, NEO_GRB + NEO_KH
 
 int x, y, z;
 int total_score = 0;   // すごろく的なやつのスコア
-int score6 = 50;   // score判定
-int score5 = 100;
-int score4 = 150;
-int score3 = 200;
-int score2 = 250;
-int score1 = 300;
+int score5 = 50;   // score判定
+int score4 = 100;
+int score3 = 150;
+int score2 = 200;
+int score1 = 250;
 int low_acceleration = 24;   // 急激な加速度の検知
 int high_acceleration = 1000;   // 急激な加速度の検知
 
@@ -46,32 +45,32 @@ void loop(){
       z_before_val = z;
     }
 
-    if(score5 < z_sum && z_sum < score6){
+    if(z_sum < score5){
       total_score += 6;
       change_ledcolor(0, 0, 255);
       return_loop();
     }
-    if(score4 < z_sum && z_sum < score5){
+    if(score5 < z_sum && z_sum < score4){
       total_score += 5;
       change_ledcolor(0, 255, 255);
       return_loop();
     }
-    if(score3 < z_sum && z_sum < score4){
+    if(score4 < z_sum && z_sum < score3){
       total_score += 4;
       change_ledcolor(0, 255, 0);
       return_loop();
     }
-    if(score2 < z_sum && z_sum < score3){
+    if(score3 < z_sum && z_sum < score2){
       total_score += 3;
       change_ledcolor(255, 255, 0);
       return_loop();
     }
-    if(score1 < z_sum && z_sum < score2){
+    if(score2 < z_sum && z_sum < score1){
       total_score += 2;
       change_ledcolor(255, 105, 180);
       return_loop();
     }
-    if(z_sum < score1){
+    if(score1 < z_sum){
       total_score += 1;
       change_ledcolor(255, 0, 0);
       return_loop();
