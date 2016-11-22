07d06ff7fa49d5263a84a1d7f188bef83f22afc9
shoichi <shou1_822@yahoo.co.jp>
2016-11-22 16:46:15 +0900

    encoder switch rotation color change

--- class_application_project.ino
+++ class_application_project.ino
@@ -1,64 +1,41 @@
 #include <Adafruit_NeoPixel.h>
 
-const int num_leds = 45;   // 制御するledの数
-const int led_pin = 6;   // led degital I/O pin
-const int flash_pin = 7;   // flash degital I/O pin
+// ピン設定
+const int acceleration_x_analogpin = 0;
+const int acceleration_y_analogpin = 1;
+const int acceleration_z_analogpin = 2;
+const int led_pin = 6;
+const int flash_pin = 7;
+const int encoder_counterclockwise_pin = 2;
+const int encoder_clockwise_pin = 4;
+const int encoder_switch_pin = 8;
+const int cyan_pin = 9;
+const int magenta_pin = 10;
+const int yellow_pin = 11;
 
-Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(num_leds, led_pin, NEO_GRB + NEO_KHZ800);
+// const int num_leds = 45;   // 制御するledの数
 
-int x, y, z;
-int small_error = 100;   // 誤差範囲の一番小さい値(Flash用の誤差の値)
-int medium_error = 125;
-int large_error = 250;
-int low_acceleration = 24;   // 急激な加速度の検知
-int high_acceleration = 1000;   // 急激な加速度の検知
+int direction_rotation = 0;   // どっちに回したか保管用
+int current_rotation = 0;   // 現在の値保管用
+int old_rotation = 0;   // 一個前の値保管用
+
+// Adafruit_NeoPixel rgbled = Adafruit_NeoPixel(num_leds, led_pin, NEO_GRB + NEO_KHZ800);
+
+// int x, y, z;
+// int small_error = 100;   // 誤差範囲の一番小さい値(Flash用の誤差の値)
+// int medium_error = 125;
+// int large_error = 250;
+// int low_acceleration = 24;   // 急激な加速度の検知
+// int high_acceleration = 1000;   // 急激な加速度の検知
 
 
-void setup(){
-  rgbled.begin();
-  for(int i=0; i<=num_leds; i++){
-    rgbled.setBrightness(50);   // 明るさ指定(0~255)
-    rgbled.setPixelColor(i, rgbled.Color(255, 255, 255));
-    rgbled.show();   // 反映
-  }
-  pinMode(flash_pin, OUTPUT);
-  digitalWrite(flash_pin, LOW);
+void setup() {
+  Serial.begin(9600);
+  pinMode(encoder_counterclockwise_pin, INPUT_PULLUP);
+  pinMode(encoder_clockwise_pin, INPUT_PULLUP);
+  pinMode(encoder_switch_pin, INPUT);
 }
 
-void loop(){
-  acceleration_read();
-
-  // フリスビー投げられた時の処理
-  if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
-    delay(100);
-    int z_before_val = analogRead(2);
-    int z_difference = 0;
-    int z_sum = 0;
-    for(int i=0; i<2; i++){
-      delay(50);
-      z = analogRead(2);
-      z_difference = abs(z_before_val - z);
-      z_sum += z_difference;
-      z_before_val = z;
-    }
-
-    if(z_sum < medium_error){
-      change_ledcolor(0, 0, 255);
-      if(z_sum < small_error){   // フラッシュ判定
-        delay(50);
-        digitalWrite(flash_pin, HIGH);
-        delay(50);
-        digitalWrite(flash_pin, LOW);
-      }
-      return_loop_normal();
-    }
-    if(medium_error < z_sum && z_sum < large_error){
-      change_ledcolor(255, 255, 0);
-      return_loop_normal();
-    }
-    if(large_error < z_sum){
-      change_ledcolor(255, 0, 0);
-      return_loop_normal();
-    }
-  }
+void loop() {
+  rotation();
 }
--- function.ino
+++ function.ino
@@ -1,121 +1,197 @@
-void acceleration_read() {
-  x = analogRead(0);
-  y = analogRead(1);
-  z = analogRead(2);
-  x = x<10 ? 0 : x;
-  x = x>1013 ? 1023 : x;
-  y = y<10 ? 0 : y;
-  y = y>1013 ? 1023 : y;
-  z = z<10 ? 0 : z;
-  z = z>1013 ? 1023 : z;
-}
+void rotation() {
+  int val_counterclockwise = digitalRead(encoder_counterclockwise_pin);
+  int val_clockwise = digitalRead(encoder_clockwise_pin);
+  int rotation_min = 0;
+  int rotation_max = 3;   // 0番目からのモード数
+  delay(1);
+  mode_color_change();
 
-void change_ledcolor(int r, int g, int b) {
-  for(int i=0; i<num_leds; i++){
-    rgbled.setPixelColor(i, rgbled.Color(r, g, b));
-    rgbled.show();
-  }
-}
-
-void flashing() {
-  delay(1000);
-  digitalWrite(flash_pin, HIGH);
-  delay(1000);
-  digitalWrite(flash_pin, LOW);
-
-}
-
-
-/* normal mode function */
-
-void return_loop_normal() {
-  while(1){
-    delay(50);
-    acceleration_read();
-    if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
-      change_ledcolor(255, 255, 255);
-      delay(200);
-      break;
+  if(val_counterclockwise == HIGH && val_clockwise == HIGH){   // 回してない時
+    if(direction_rotation == 1){   // 一回前に右回りしてたら
+      current_rotation += 1;
+      if(current_rotation > rotation_max){
+        current_rotation = rotation_max;
+      }
+    }
+    if(direction_rotation == -1){   // 一回前に左回りしてたら
+      current_rotation -= 1;
+      if(current_rotation < rotation_min){
+        current_rotation = rotation_min;
+      }
+    }
+    direction_rotation = 0;   // direction_rotationの値に動かしてないことを保管
+  } else {   // どっちかに回転したら
+    if(val_counterclockwise == LOW){   // 右回りしたら
+      direction_rotation = 1;   // direction_rotationの値に右回りしたことを保管
+    }
+    if(val_clockwise == HIGH){   // 左回りしたら
+      direction_rotation = -1;   // direction_rotationの値に左回りしたことを保管
     }
   }
+  if(old_rotation != current_rotation){   // 前の値と今の値が異なったら
+    old_rotation = current_rotation;   // 一回前の値を格納する
+    Serial.println(old_rotation);
+  }
 }
 
+void mode_color_change() {
+  switch(current_rotation){
+    case 0: //RED
+      analogWrite(cyan_pin, 0);
+      analogWrite(magenta_pin, 255);
+      analogWrite(yellow_pin, 255);
+      break;
+    case 1: //GREEN
+      analogWrite(cyan_pin, 255);
+      analogWrite(magenta_pin, 0);
+      analogWrite(yellow_pin, 255);
+      break;
+    case 2: //YELLOW
+      analogWrite(cyan_pin, 0);
+      analogWrite(magenta_pin, 0);
+      analogWrite(yellow_pin, 255);
+      break;
+    case 3: //BLUE
+      analogWrite(cyan_pin, 255);
+      analogWrite(magenta_pin, 255);
+      analogWrite(yellow_pin, 0);
+      break;
+    case 4: //PINK
+      analogWrite(cyan_pin, 0);
+      analogWrite(magenta_pin, 255);
+      analogWrite(yellow_pin, 0);
+      break;
+    case 5: //SKY BLUE
+      analogWrite(cyan_pin, 255);
+      analogWrite(magenta_pin, 0);
+      analogWrite(yellow_pin, 0);
+      break;
+    case 6: //WHITE
+      analogWrite(cyan_pin, 0);
+      analogWrite(magenta_pin, 0);
+      analogWrite(yellow_pin, 0);
+      break;
+  }
+}
 
-// /* art mode function */
+// void acceleration_read() {
+//   x = analogRead(0);
+//   y = analogRead(1);
+//   z = analogRead(2);
+//   x = x<10 ? 0 : x;
+//   x = x>1013 ? 1023 : x;
+//   y = y<10 ? 0 : y;
+//   y = y>1013 ? 1023 : y;
+//   z = z<10 ? 0 : z;
+//   z = z>1013 ? 1023 : z;
+// }
 
-// void change_rgb(int current_H, int H){
-//   for(int i=0; current_H<=H; current_H++){
-//     if(current_H <= 120) {
-//       R = map(current_H, 0, 120, 255, 0);
-//       G = map(current_H, 0, 120, 0, 255);
-//       B = 0;
-//     } else if (current_H <= 240) {
-//       G = map(current_H, 120, 240, 255, 0);
-//       B = map(current_H, 120, 240, 0, 255);
-//       R = 0;
-//     } else {
-//       B = map(current_H, 240, 360, 255, 0);
-//       R = map(current_H, 240, 360, 0, 255);
-//       G= 0;
-//     }
-//     change_ledcolor(R, G, B);
-//     delay(3);
+// void change_ledcolor(int r, int g, int b) {
+//   for(int i=0; i<num_leds; i++){
+//     rgbled.setPixelColor(i, rgbled.Color(r, g, b));
+//     rgbled.show();
 //   }
 // }
 
+// void flashing() {
+//   delay(1000);
+//   digitalWrite(flash_pin, HIGH);
+//   delay(1000);
+//   digitalWrite(flash_pin, LOW);
 
-// /* fugoroku mode function */
+// }
 
-// void return_loop_sugoroku() {
+
+// /* normal mode function */
+
+// void return_loop_normal() {
 //   while(1){
 //     delay(50);
 //     acceleration_read();
 //     if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
-//       score_led();
+//       change_ledcolor(255, 255, 255);
 //       delay(200);
 //       break;
 //     }
 //   }
 // }
 
-// void score_led() {
-//   delay(500);
-//   total_score = total_score>num_leds ? total_score-num_leds : total_score;
-//   if(total_score < num_leds){
-//     for(int i=0; i<total_score; i++){
-//       rgbled.setPixelColor(i, rgbled.Color(255, 0, 0));
-//       rgbled.show();
-//     }
-//     for(int i=total_score; i<num_leds; i++){
-//       rgbled.setPixelColor(i, rgbled.Color(0, 0, 0));
-//       rgbled.show();
-//     }
-//   }
-//   if(total_score == num_leds){
-//     delay(50);
-//     digitalWrite(flash_pin, HIGH);
-//     delay(50);
-//     digitalWrite(flash_pin, LOW);
-//   }
-// }
+
+// // /* art mode function */
+
+// // void change_rgb(int current_H, int H){
+// //   for(int i=0; current_H<=H; current_H++){
+// //     if(current_H <= 120) {
+// //       R = map(current_H, 0, 120, 255, 0);
+// //       G = map(current_H, 0, 120, 0, 255);
+// //       B = 0;
+// //     } else if (current_H <= 240) {
+// //       G = map(current_H, 120, 240, 255, 0);
+// //       B = map(current_H, 120, 240, 0, 255);
+// //       R = 0;
+// //     } else {
+// //       B = map(current_H, 240, 360, 255, 0);
+// //       R = map(current_H, 240, 360, 0, 255);
+// //       G= 0;
+// //     }
+// //     change_ledcolor(R, G, B);
+// //     delay(3);
+// //   }
+// // }
 
 
-// /* bomb mode function */
+// // /* fugoroku mode function */
 
-// void return_loop_bomb() {
-//   while(1){
-//     delay(50);
-//     acceleration_read();
-//     if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
-//       if(total_score >= over_score){
-//         delay(50);
-//         digitalWrite(flash_pin, HIGH);
-//         delay(50);
-//         digitalWrite(flash_pin, LOW);
-//         total_score = 0;
-//       }
-//       delay(200);
-//       break;
-//     }
-//   }
-// }
+// // void return_loop_sugoroku() {
+// //   while(1){
+// //     delay(50);
+// //     acceleration_read();
+// //     if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
+// //       score_led();
+// //       delay(200);
+// //       break;
+// //     }
+// //   }
+// // }
+
+// // void score_led() {
+// //   delay(500);
+// //   total_score = total_score>num_leds ? total_score-num_leds : total_score;
+// //   if(total_score < num_leds){
+// //     for(int i=0; i<total_score; i++){
+// //       rgbled.setPixelColor(i, rgbled.Color(255, 0, 0));
+// //       rgbled.show();
+// //     }
+// //     for(int i=total_score; i<num_leds; i++){
+// //       rgbled.setPixelColor(i, rgbled.Color(0, 0, 0));
+// //       rgbled.show();
+// //     }
+// //   }
+// //   if(total_score == num_leds){
+// //     delay(50);
+// //     digitalWrite(flash_pin, HIGH);
+// //     delay(50);
+// //     digitalWrite(flash_pin, LOW);
+// //   }
+// // }
+
+
+// // /* bomb mode function */
+
+// // void return_loop_bomb() {
+// //   while(1){
+// //     delay(50);
+// //     acceleration_read();
+// //     if(x<low_acceleration || high_acceleration<x || y<low_acceleration || high_acceleration<y){
+// //       if(total_score >= over_score){
+// //         delay(50);
+// //         digitalWrite(flash_pin, HIGH);
+// //         delay(50);
+// //         digitalWrite(flash_pin, LOW);
+// //         total_score = 0;
+// //       }
+// //       delay(200);
+// //       break;
+// //     }
+// //   }
+// // }
